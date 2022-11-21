6815efe98d6b57471607060faefb701380d5a8d4
Ibrahim Ali <84272197+ibrahiim1@users.noreply.github.com>
2022-11-21 10:39:23 +0200

    Add files via upload

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
-# From project 4 resources
+import os 
 import json
+import logging
 import sys
-import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,93 +10,70 @@ import torchvision.transforms as transforms
 from PIL import Image
 import io
 import requests
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 JSON_CONTENT_TYPE = 'application/json'
 JPEG_CONTENT_TYPE = 'image/jpeg'
 
-try:
-    import smdebug
-except:
-    pass
-
-# Based on https://github.com/pytorch/examples/blob/master/mnist/main.py
-def Net():
-    model = models.resnet50(pretrained=True)
-
-    for param in model.parameters():
-        param.requires_grad = False 
-        
-    model.fc = nn.Sequential(nn.Linear(model.fc.in_features, 133))
-    return model
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-
 def model_fn(model_dir):
-    print("In model_fn. Model directory is -")
-    print(model_dir)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = Net().to(device)
     
-    with open(os.path.join(model_dir, "model_2.pt"), "rb") as f:
-        print("Loading the dog-classifier model")
-        checkpoint = torch.load(f, map_location=device)
-        model.load_state_dict(checkpoint)
+    print(model_dir)
+    
+    model = models.resnet50(pretrained=True)
+ 
+    for param in model.parameters():
+        param.requires_grad = False
+        
+    num_features = model.fc.in_features
+    model.fc = nn.Sequential(nn.Linear(num_features, 133))
+    
+    model = model.to(device)
+    
+     
+    with open(os.path.join(model_dir, "model_2.pt"), 'rb') as f:
+        model.load_state_dict(torch.load(f, map_location = device),strict=False)
         print('MODEL-LOADED')
-        print('model loaded successfully')
+
     model.eval()
+
     return model
 
 
 
 
-def input_fn(request_body, content_type=JPEG_CONTENT_TYPE):
-    print('Deserializing the input data.')
+def input_fn(request_body, content_type='image/jpeg'):
+    
+    logger.info('Deserializing the input data.')
     # process an image uploaded to the endpoint
     #if content_type == JPEG_CONTENT_TYPE: return io.BytesIO(request_body)
-    print(f'Request body CONTENT-TYPE is: {content_type}')
-    print(f'Request body TYPE is: {type(request_body)}')
-    if content_type == JPEG_CONTENT_TYPE: return Image.open(io.BytesIO(request_body))
-    print('SO loded JPEG content')
-    # process a URL submitted to the endpoint
-    
-    if content_type == JSON_CONTENT_TYPE:
-        #img_request = requests.get(url)
-        print(f'Request body is: {request_body}')
-        request = json.loads(request_body)
-        print(f'Loaded JSON object: {request}')
-        url = request['url']
-        img_content = requests.get(url).content
-        return Image.open(io.BytesIO(img_content))
+    logger.debug(f'Request body CONTENT-TYPE is: {content_type}')
+    logger.debug(f'Request body TYPE is: {type(request_body)}')
+    logger.debug(f'Request length is: {len(request_body)}')
+    logger.debug(f'Request body is: {request_body}')
+    if content_type == JPEG_CONTENT_TYPE: 
+#         request_body.seek()
+        return Image.open(io.BytesIO(request_body))
+    logger.debug('SO loded JPEG content')
     
     raise Exception('Requested unsupported ContentType in content_type: {}'.format(content_type))
 
 # inference
 def predict_fn(input_object, model):
-    print('In predict fn')
-    test_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    ])
-    print("transforming input")
+    logger.info('In predict fn')
+    test_transform = transforms.Compose([transforms.Resize(255), transforms.CenterCrop(224),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize((0.485, 0.456, 0.406),
+                                                          (0.229, 0.224, 0.225))])
+
+    logger.info("transforming input")
     input_object=test_transform(input_object)
     
     with torch.no_grad():
-        print("Calling model")
+        logger.info("Calling model")
         prediction = model(input_object.unsqueeze(0))
     return prediction
-
-'''
-# postprocess
-def output_fn(predictions, content_type):
-    assert content_type == "application/json"
-    res = predictions.cpu().numpy().tolist()
-    return json.dumps(res)
-# Serialize the prediction result into the desired response content type
-def output_fn(prediction, accept=JSON_CONTENT_TYPE):        
-    logger.info('Serializing the generated output.')
-    if accept == JSON_CONTENT_TYPE: 
-        logger.debug(f'Returning response {json.dumps(prediction)}')
-        return json.dumps(prediction), accept
-    raise Exception('Requested unsupported ContentType in Accept: {}'.format(accept))
-'''
